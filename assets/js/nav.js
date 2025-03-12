88b81c3a53d4dc0712d1738164e53e5ae3e1a5df
jose.gomez <javierrumo2@gmail.com>
2025-03-12 21:48:26 +0100

    minifiles

---
+++ assets/js/nav.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){const navLinks=document.querySelectorAll("#nav ul li a");const currentPath=window.location.pathname;navLinks.forEach((link)=>{if(link.href.includes(currentPath)){link.classList.add("active");}else{link.classList.remove("active");}});});
---
+++ assets/js/traducirTexto.js
@@ -0,0 +1,2 @@
+document.addEventListener('DOMContentLoaded',()=>{const espana=document.getElementById('espana');const ingles=document.getElementById('ingles');if(localStorage.getItem('lang')){translate(localStorage.getItem('lang'));}
+espana.addEventListener('click',()=>translate('es'));ingles.addEventListener('click',()=>translate('en'));});function translate(lang){localStorage.setItem('lang',lang);const elements=document.querySelectorAll('[data-es], [data-en]');elements.forEach(element=>{element.textContent=element.getAttribute(`data-${lang}`);});}
