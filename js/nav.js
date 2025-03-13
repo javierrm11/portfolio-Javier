58addae43b2e693a0e62b661f287b88badb1da53
jose.gomez <javierrumo2@gmail.com>
2025-03-13 08:50:25 +0100

    fix bug nav

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelectorAll("#nav ul li a");
 
-    // Obtener la URL actual
-    const currentPath = window.location.pathname;
+    // Obtener la URL actual sin el dominio
+    let currentPath = window.location.pathname;
 
     navLinks.forEach((link) => {
-        if (link.href.includes(currentPath)) {
+        // Extraer solo el pathname del enlace (sin el dominio)
+        let linkPath = new URL(link.href).pathname;
+
+        // Caso especial para la página de inicio
+        if ((currentPath === "/" || currentPath === "/index.html") && (linkPath === "/" || linkPath === "/index.html")) {
+            link.classList.add("active");
+        } else if (currentPath !== "/" && currentPath === linkPath) {
             link.classList.add("active");
         } else {
             link.classList.remove("active");
