3e4052b18b4b693e46f474fe6299993dacddd1b5
Javier <javierrumo2@gmail.com>
2025-10-04 11:02:43 +0200

    Ejercicio terminado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://javierm.dev', // 👈 muy importante para que el sitemap funcione
+  integrations: [sitemap()],
+});
