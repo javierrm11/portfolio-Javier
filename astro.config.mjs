0725e6706d53a887264ea95de74002947e2cdcfa
Javier <javierrumo2@gmail.com>
2026-09-25 08:36:17 +0200

    update portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://javierm.dev', // 👈 muy importante para que el sitemap funcione
+  site: 'https://www.javierm.dev', // 👈 muy importante para que el sitemap funcione
   integrations: [sitemap()],
 });
